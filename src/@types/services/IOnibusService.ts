bce47a8a0af2303ff45bf046c804a06ccb757e42
eriking50 <eriking5@hotmail.com>
2021-11-30 20:26:23 -0300

    atualizando interface do service de onibus

--- src/@types/services/IOnibusService.ts
+++ src/@types/services/IOnibusService.ts
@@ -1,3 +1,9 @@
+import { OnibusAtualizarDto, OnibusDto } from "../dto/OnibusDto";
+import { Onibus } from "../../models/OnibusEntity";
+
 export interface IOnibusService {
-    
+  criarOnibus(dadosOnibus: OnibusDto): Promise<Onibus>;
+  atualizarOnibus(idOnibus: number, dadosOnibus: OnibusAtualizarDto): Promise<void>
+  buscarOnibus(idOnibus: number): Promise<Onibus>;
+  listarOnibus(): Promise<Onibus[]>;
 }
