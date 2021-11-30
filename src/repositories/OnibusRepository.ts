c380cf0ea5ad55293a160bd7dd113ec7a06c21f0
eriking50 <eriking5@hotmail.com>
2021-11-30 20:19:57 -0300

    Criando service de onibus

--- src/repositories/OnibusRepository.ts
+++ src/repositories/OnibusRepository.ts
@@ -5,4 +5,14 @@ import { IOnibusRepository } from "../@types/repositories/IOnibusRepository";
 
 @EntityRepository(Onibus)
 export class OnibusRepository extends Repository<Onibus> implements IOnibusRepository{
+  findById(idOnibus: number): Promise<Onibus> {
+    return this.findOne({
+      where: {
+        id: idOnibus
+      }
+    })
+  }
+  findAll(): Promise<Onibus[]> {
+    return this.find();
+  }
 }
